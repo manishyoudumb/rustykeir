84d37bbb8adc992b78b4c0319bdb0e84fea2658e
manishyoudumb <darksalterego99@gmail.com>
2024-07-18 19:31:39 +0530

    Added lexer module for Keir language

---
+++ src/lexer.rs
@@ -0,0 +1,33 @@
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    Number(f64),
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    LeftParen,
+    RightParen,
+    EOF,
+
+}
+
+pub struct Lexer<'a> {
+    input: &'a str,
+    position: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Lexer { input, position: 0 }
+    }
+
+    pub fn +
+
+
+
+
+}
+
+
+
