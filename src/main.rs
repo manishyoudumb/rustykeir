ece323bb0dfa40d8a66270d9d023859f77a88a30
manishyoudumb <darksalterego99@gmail.com>
2024-07-19 20:14:45 +0530

    Modified `main.rs` to handle file input in the `interpreter`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 mod ast;
 mod lexer;
@@ -8,10 +8,19 @@ mod interpreter;
 
 
 
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     clearscreen::clear().expect("Aww, We failed to clear your screen ");
     let args: Vec<String> = env::args().collect();
 
-    if args
+    if args.len() > 1 {
+        let filename = &args[1];
+        let content = fs::read_to_string(filename)?;
+        run(content)
+    }
+    else {
+        repl()
+    }
 
 }
+
