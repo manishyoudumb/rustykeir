60cbb780903ddd0ecdb2ff47b52f41424854791b
manishyoudumb <darksalterego99@gmail.com>
2024-07-19 20:05:08 +0530

    Add `clearscreen` crate and clear screen before running the `interpreter`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::env;
+
 mod ast;
 mod lexer;
 mod error;
 mod parser;
 mod interpreter;
 
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    clearscreen::clear().expect("Aww, We failed to clear your screen ");
+    let args: Vec<String> = env::args().collect();
+
+    if args
+
 }
