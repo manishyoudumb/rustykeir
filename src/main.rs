a770c54ca4d3d6ea6f0485562f65b61df54543c5
manishyoudumb <darksalterego99@gmail.com>
2024-07-19 22:28:59 +0530

    Added `REPL` functionality to `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ mod interpreter;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 use crate::interpreter::Interpreter;
+use rustyline::error::ReadlineError;
+use crate::error::KeirError;
+use rustyline::Editor;
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -39,3 +42,58 @@ fn run(source: String) -> Result<(), Box<dyn std::error::Error>> {
     println!("Result: {}", result);
     Ok(())
 }
+
+
+fn repl() -> Result<(), Box<dyn std::error::Error>> {
+    let mut rl = Editor::<()>::new();
+    let interpreter = Interpreter::new();
+
+println!("|---------------------------------------------------------------------|");
+println!("| Welcome to Keir Lang.                                               |");
+println!("|---------------------------------------------------------------------|");
+println!("| Type 'exit' or 'quit' to leave.                                     |");
+println!("|---------------------------------------------------------------------|");
+println!("|ᵛᵉʳˢⁱᵒⁿ 0.1.o                                   ᵃᵘᵗʰᵒʳ: manishyoudumb|");
+println!("|---------------------------------------------------------------------|");
+
+    loop {
+        let readline = rl.readline("admin@keir->> ");
+        match readline {
+            Ok(line) => {
+                if line.trim() == "exit" || line.trim() == "quit" {
+                    break;
+                }
+                rl.add_history_entry(line.as_str());
+                match eval(&line, &interpreter) {
+                    Ok(result) => println!("Output is : {}", result),
+                    Err(e) => eprintln!("Error : {}", e),
+
+                }
+                
+            },
+            
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            },
+
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            },
+            
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+
+
+        }
+    }
+    println!("Miss me and be back soon!");
+    Ok(())
+}
+
+
+
+
